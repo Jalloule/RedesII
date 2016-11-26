861f0087a26d7186af89d43ce9a423fa703764ba
gvpm <gvillar@id.uff.br>
2016-11-26 11:28:29 -0200

    File Transfer working. - Server asks for output name and waits for connection. - Client asks for hostname, fileToSent name and sends the file. - Client ends. - Server receives the file, writes the file and ends.

--- src/Client/TCPClient.java
+++ src/Client/TCPClient.java
@@ -9,41 +9,66 @@ public class TCPClient {
     public static void run() throws Exception {
         //Port and adress to connect to the server
         int port = 6789;
-        
         String address;
+        String fileToSend;
+        //gets the host address to connect
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter the host address: ");
         address = reader.nextLine();
-
-        String sentence;
-        String modifiedSentence;
-
+        //control messages
         System.out.println("Client is On");
         System.out.println("Will sent to Port:" + port + " on: " + address);
 
-        while (true) {
-            //Reader to read the user input
-            System.out.println("Write message and press ENTER");
-            
-            // Reading from users keyboars input
-            String inFromUser = reader.nextLine();
-            System.out.println("");
+        //Stream to send to server
+        BufferedOutputStream outToServer = null;
 
-            try (Socket clientSocket = new Socket(address, port)) {
-                //Creates stream to send message to server
-                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-                //BufferedReader to read servers answer
-                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        //Reader to read the user input
+        System.out.println("\nWrite the name of the file to send: ");
 
-                //Puts the user input on variable sentence
-                sentence = inFromUser;
-                //Sends the message to server
-                outToServer.writeBytes(sentence + '\n');
-                System.out.println("Sent to Server: " + sentence);
-                //Gets message from server
-                modifiedSentence = inFromServer.readLine();
-                System.out.println("Received from Server: " + modifiedSentence + "\n");
+        // Reading from users keyboars input
+        fileToSend = reader.nextLine();
+        System.out.println("");
+
+        try {//tries to connect to server
+            //Creates stream to send message to server
+            Socket clientSocket = new Socket(address, port);
+            outToServer = new BufferedOutputStream(clientSocket.getOutputStream());
+
+        } catch (IOException ex) {
+            System.out.println("Could not connect to server");
+        }
+        if (outToServer != null) {
+            //Creates the file to send
+            File myFile = new File(fileToSend);
+            //byteArray to read the file to
+            byte[] mybytearray = new byte[(int) myFile.length()];
+
+            FileInputStream fis = null;
+
+            try {
+                fis = new FileInputStream(myFile);
+            } catch (FileNotFoundException ex) {
+                // Do exception handling
+            }
+            //Stream that will get the array to send to the server
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            try {
+                
+                bis.read(mybytearray, 0, mybytearray.length);
+                outToServer.write(mybytearray, 0, mybytearray.length);
+                //sends to server
+                outToServer.flush();
+                outToServer.close();
+                //clientSocket.close();
+
+                // File sent, exit the main method
+                System.out.println("\nFile " + fileToSend + " was sent successfully");
+                return;
+            } catch (IOException ex) {
+                System.out.println("Could not send the file");
             }
         }
+        
     }
 }
--- src/Client/TCPServer.java
+++ src/Client/TCPServer.java
@@ -2,36 +2,73 @@ package Client;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class TCPServer {
 
     public static void run() throws Exception {
-        //Port to open the server
-        int port = 6789;
+        //Some variables
+        byte[] aByte = new byte[1];//array of bytes so put the input in
+        int bytesRead;
+        String fileOutput;//nameof the outFile
+        int port = 6789;//Port to open the server
 
-        String clientSentence;
-        String capitalizedSentence;
+        InputStream is = null;
+
+        Scanner reader = new Scanner(System.in);
+
         //Opens the socket
         ServerSocket welcomeSocket = new ServerSocket(6789);
         //Control messages
         System.out.println("Socked opened");
         System.out.println("Server is On");
         System.out.println("Listening on Port:" + port + "\n");
-        //Server loop, will wait for messages
-        while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            //BufferedReader to read clients input
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            //Creates stream to answer the client
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-            //Reads the client message from bufferedReader
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received from Client: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            //Sends the message back to client, capitalized
-            outToClient.writeBytes(capitalizedSentence);
-            System.out.println("Sent to Client: " + capitalizedSentence);
+        //Reader to read the user input
+        System.out.println("Write the name of the output file: ");
+
+        // Reading from users keyboars input
+        fileOutput = reader.nextLine();
+        System.out.println("");
+        
+        //Server loop, will wait for messages
+        System.out.println("\nWaiting to receive a file from the Client...");
+        while (true) {
+            //Connection socket openned
+            Socket connectionSocket = welcomeSocket.accept();
+            //Input stream to read the file
+            is = connectionSocket.getInputStream();
+            //Output stream to write the file
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            if (is != null) {
+
+                FileOutputStream fos = null;
+                BufferedOutputStream bos = null;
+                try {
+                    fos = new FileOutputStream(fileOutput);
+                    bos = new BufferedOutputStream(fos);
+                    //reads from the input stream, from the client
+                    bytesRead = is.read(aByte, 0, aByte.length);
+
+                    do {
+                        //writes  a byte on the out stream to the file
+                        baos.write(aByte);
+                        //reads a byte form the input
+                        bytesRead = is.read(aByte);
+                    } while (bytesRead != -1);//will do this until thereis something to read
+                    //once it is done
+                    bos.write(baos.toByteArray());//writes the file
+                    bos.flush();
+                    bos.close();//closes the output file
+                    connectionSocket.close();//closes the connection
+                    System.out.println("\nFile received successfully and saved as " + fileOutput);
+                    return;//leaves the loop and closes the server
+                } catch (IOException ex) {
+                    System.out.println("Error in writing the file");
+                }
+            }
+
         }
     }
 }
